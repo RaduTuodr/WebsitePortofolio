76dbf749b4d0372d06d33c1a085bfbbeaec92cac
RaduTuodr <tudoreduard2004@gmail.com>
2024-04-10 13:23:55 +0300

    Intro + AboutMe sections

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import IntroSection from "../components/IntroSection";
+import Navbar from "../components/Navbar";
+import AboutMeSection from "../components/AboutMeSection"
+
+export default function Home() {
+
+	return (
+		<main className="flex min-h-screen flex-col bg-[#121212] container">
+
+			<Navbar />
+
+			<div className="container mx-auto mt-24 px-12 py-4">
+				<IntroSection />
+			</div>
+
+			<AboutMeSection/>
+
+		</main>
+	)
+}
